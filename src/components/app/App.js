6b481c26141f75f686e9f6df1fa7f1f909190d1a
ChandanaRD <deshmukh.chandana@gmail.com>
2020-11-05 00:19:40 +0530

    header

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import Header from '../header/Header';
+import Container from '../container/Container';
+import Footer from '../footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  makeStyles,
+  createMuiTheme,
+  MuiThemeProvider,
+} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import '../../colors.scss';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#bf360c',
+    },
+    secondary: {
+      main: '#ffd180',
+    },
+  },
+});
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Home
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+export default Header;
